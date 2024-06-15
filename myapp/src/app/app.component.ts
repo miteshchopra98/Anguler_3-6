b0d8faedfc475d0b2b9f836e56d5660c58ec13fb
Mitesh <miteshchopra98@gmail.com>
2024-06-15 15:43:41 +0530

    Commit - 2

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { truncate } from 'node:fs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  a: number = 12
+  b: string = 'oreo'
+  c:boolean = true
+  d:string[] = ['Mitesh', 'Chopra']
+  e:number[] = [1,2,3]
+  
 }
