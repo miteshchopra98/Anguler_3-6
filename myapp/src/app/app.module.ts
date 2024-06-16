335b2ab64f5ce0a4adf72c584c1ffdf9dc2c2b2c
Mitesh <miteshchopra98@gmail.com>
2024-06-16 18:19:20 +0530

    Commit - 3

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NvbarComponent } from './nvbar/nvbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NvbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ myapp/src/app/nvbar/nvbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './nvbar.component.html',
+  styleUrl: './nvbar.component.css'
+})
+export class NvbarComponent {
+  
+}
